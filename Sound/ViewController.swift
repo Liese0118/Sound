b0427221f085e740dd1c7d3c882a4cfe3eb9a2b5
Liese Uriu <liese.c.uriu@gmail.com>
2019-11-27 13:39:37 +0900

    Mozer教材１

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -7,13 +7,36 @@
 //
 
 import UIKit
+import AVFoundation //音楽をならしたいときに必要
 
 class ViewController: UIViewController {
+    //MainStoryboardで作成したボタンをコードでも扱えるように
+    @IBOutlet var button: UIButton!
+    //音楽をならす源の役割を担う
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func tapbeginButton(){
+        let path = Bundle.main.path(forResource: "guitar", ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        audioPlayer = try! AVAudioPlayer(contentsOf: url)
+        audioPlayer.play()
+        
+        //ぼボタンが押されている間は写真が猫になる
+        button.setImage(UIImage(named:"musician_cat.png"), for: .normal)
+        
+        print("tap begin")
+        
+    }
+    @IBAction func tapEndButton(){
+        button.setImage(UIImage(named:"guitar.png"), for: .normal)
+         print("tap end")
+    }
+    
+   
 
 
 }
